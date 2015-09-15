ea9b7ccf28d7c08b3546b74eb651ae6abdde0a33
Md Amran Siddiqui <amransd@gmail.com>
2015-09-15 14:58:04 -0700

    added OnlineIF class

---
+++ OnlineIF.cpp
@@ -0,0 +1,58 @@
+#include "OnlineIF.hpp"
+
+OnlineIF::OnlineIF(int _ntree, doubleframe* _df, int _nsample, int _maxheight,
+		bool _stopheight, bool _rsample, int _windowSize) :
+		Forest(_ntree, _nsample, _maxheight, _stopheight, _rsample) {
+
+	this->windowSize = _windowSize;
+	this->updateCount = 0;
+	genInitTreeStructures(_df);
+}
+
+/*
+ * Generate initial tree structures from offline isolation
+ * forest using first windowSize instances
+ */
+void OnlineIF::genInitTreeStructures(doubleframe *dataset){
+	std::vector<int> sampleIndex;
+	for (int n = 0; n < this->ntree; n++) {
+		// Sample and shuffle the data.
+		sampleIndex.clear();
+		getSample(sampleIndex, nsample, rsample, windowSize);
+
+		// build trees
+		Tree *tree = new Tree();
+		tree->iTree(sampleIndex, dataset, 0, maxheight, stopheight);
+		this->trees.push_back(tree); //add tree to forest
+	}
+}
+
+double OnlineIF::instanceScore(double *inst) {
+	double avgPathLength = mean(pathLength(inst));
+//	double scores = pow(2, -avgPathLength / avgPL(this->nsample));
+//	return scores;
+	return avgPathLength;
+}
+
+
+/*
+ * Update corresponding nodes in the trees for a new instance
+ */
+void OnlineIF::update(double inst[]){
+	double p = this->nsample/double(this->windowSize);
+	for(int i = 0; i < this->ntree; ++i){
+		if(randomD(0, 1) < p)
+			this->trees[i]->update(inst);
+	}
+
+	this->updateCount++;
+
+	// After performing windowSize updates renew trees with new nodeSizes
+	if(this->updateCount == this->windowSize){
+		for(int i = 0; i < this->ntree; ++i){
+			this->trees[i]->renewNodeSize();
+		}
+		// reset update count to start again
+		this->updateCount = 0;
+	}
+}
---
+++ OnlineIF.hpp
@@ -0,0 +1,20 @@
+#ifndef ONLINEIF_HPP_
+#define ONLINEIF_HPP_
+
+#include "Forest.hpp"
+
+class OnlineIF: public Forest {
+	int windowSize;
+	int updateCount;
+
+	void genInitTreeStructures(doubleframe *dataset);
+public:
+	OnlineIF(){}
+	OnlineIF(int _ntree, doubleframe* _df, int _nsample, int _maxheight,
+			bool _stopheight, bool _rsample, int _windowSize);
+	virtual ~OnlineIF() {}
+	double instanceScore(double *inst);
+	void update(double inst[]);
+};
+
+#endif /* ONLINEIF_HPP_ */
